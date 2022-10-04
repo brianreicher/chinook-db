bf506a8b8a5b32e7c5b523be00a18656fbd4523b
brianreicher <reicher.b@northeastern.edu>
2022-10-03 23:36:42 -0400

    Full query build

--- queries/chinook.sql
+++ queries/chinook.sql
@@ -8,10 +8,9 @@ SELECT al.title as AlbumTitle, ar.name as ArtistName
 FROM albums al JOIN  artists ar on al.ArtistId = ar.ArtistId JOIN tracks t
     on al.AlbumId = t.AlbumId
 GROUP BY AlbumTitle
--- Where COUNT(distinct(GenreId)) > 1;
-having COUNT(distinct(GenreId)) > 1
+HAVING COUNT(distinct(GenreId)) > 1
+ORDER BY ArtistName;
 
-ORDER BY AlbumTitle;
 
 /*
  2. In which countries did we bill for videos from the Drama genre?
@@ -24,6 +23,7 @@ FROM customers JOIN invoices i on customers.CustomerId = i.CustomerId JOIN
 WHERE g.Name = 'Drama'
 ORDER BY Country;
 
+
 /*
  3. How many tracks are associated with each genre? Provide the Genre Name and number of tracks.
     Sort the list by number of tracks in descending order.
@@ -33,6 +33,7 @@ SELECT COUNT(t.Name) as Number, g.Name
 FROM tracks t join albums a on a.AlbumId = t.AlbumId join genres g on g.GenreId = t.GenreId
 GROUP BY g.Name
 ORDER BY Number DESC;
+
 
 /*
  4. A new customer LOVES things composed by Steven Tyler but is not a fan of Joe Perry compositions.
@@ -46,11 +47,39 @@ FROM tracks tr JOIN albums al on al.AlbumId = tr.AlbumId
 WHERE Composer like '%Steven Tyler%' and Composer not like '%Joe Perry%'
 ORDER BY TrackName; --default is ASC
 
+
 /*
  5. Which albums have above-average total play time?
  Provide a list of album titles with the artist's name in alphabetic order by Album name.
  */
-SELECT AVG(AlbumTimeAvg)
-FROM (SELECT AVG(Milliseconds) as AlbumTimeAvg
-FROM tracks t JOIN albums a on a.AlbumId = t.AlbumId
-GROUP BY a.Title);
+
+SELECT SUM(Milliseconds) as AlbumTotalLength, Title as AlbumTitle, a2.Name as ArtistName
+FROM albums join tracks t on albums.AlbumId = t.AlbumId join artists a2 on albums.ArtistId = a2.ArtistId
+GROUP BY Title
+HAVING AlbumTotalLength >
+(SELECT AVG(AlbumTotal) as AvgAlbumTotalLength
+    FROM (SELECT SUM(Milliseconds) as AlbumTotal
+    FROM tracks t JOIN albums a on a.AlbumId = t.AlbumId
+    GROUP BY a.Title))
+ORDER BY AlbumTitle;
+
+
+/*
+ 6. Write a query to help Margaret Park, one of the Chinook sales representatives:
+ she just closed a big sale (over $10!!) and was about to celebrate, but the customer called with a question:
+ “Margaret, mon amie, do you recall the composer of that track?
+ It was either the . . . Jazz or Classical one?”
+ Margaret is so shocked, she has forgotten who the customer is, and so can’t lookup the track/invoice directly;
+ and of course, she doesn’t want to ask the customer’s name just after a big sale . . . eek!
+ Fortunately, the customer used a bit of French, and so Margaret remembers the customer is from France.
+ You are to write a query to answer the customer’s question using only the information supplied.
+ You cannot hardcode any numeric ids (e.g., EmployeeId, GenreId, CustomerId, SupportRepId, TrackId),
+ nor identifying information about the track/invoice/etc other than the criteria described above.
+ */
+
+SELECT DISTINCT t.Composer as ForgottenComposer
+FROM employees JOIN customers c on employees.EmployeeId = c.SupportRepId JOIN
+    invoices i on c.CustomerId = i.CustomerId JOIN invoice_items ii on i.InvoiceId = ii.InvoiceId
+    JOIN tracks t on t.TrackId = ii.TrackId JOIN genres g on g.GenreId = t.GenreId
+WHERE employees.FirstName = 'Margaret' and employees.LastName = 'Park'
+  and i.Total>10 and i.BillingCountry = 'France' and g.Name in ('Jazz', 'Classical') and ForgottenComposer IS NOT NULL;
